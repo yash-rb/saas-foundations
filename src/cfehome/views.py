ca8cf0a872f4ae97bab1e19b5898108acf5d482e
Yash Rajbhar <yashraj78289@gmail.com>
2024-09-07 23:43:54 +0530

    Created home page for cfe home

---
+++ src/cfehome/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    
+    html_template = "home.html"
+    return render(request, html_template, my_context)
