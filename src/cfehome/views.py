35f9f3ce835faba980fab28c587d3810b580f432
Yash Rajbhar <yashraj78289@gmail.com>
2024-09-08 00:41:52 +0530

    added functionality to view visit count on page

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from visits.models import PageVisit
 
 
 def home_page_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
     page_title = "Homepage"
     my_context = {
-        "page_title": page_title
+        "page_title": page_title,
+        "page_visit_count":page_qs.count(),
+        "total_visit_count": qs.count(),
     }
+    path = request.path
+    PageVisit.objects.create(path=request.path)
     
     html_template = "home.html"
     return render(request, html_template, my_context)
