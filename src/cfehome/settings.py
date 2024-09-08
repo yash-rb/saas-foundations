9ac30235e49a63ef676fc402dfb71a41b4206db9
Yash Rajbhar <yashraj78289@gmail.com>
2024-09-08 11:20:04 +0530

    Added custom static files/ folder

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -141,7 +141,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR /"staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR /"vendor"
 
+# sources for for python manage.py collectstatic
+STATICFILES_DIRS=[
+    STATICFILES_BASE_DIR,
+    
+]
+
+# output for python manage.py collectstatic
+STATIC_ROOT = BASE_DIR.parent / "local-cdn"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
