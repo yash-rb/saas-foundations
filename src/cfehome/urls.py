4fb99b973403bb0dbb89977bab51e887dbd677af
Yash Rajbhar <yashraj78289@gmail.com>
2024-09-10 10:47:52 +0530

    Modifying the navbar links

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -21,7 +21,7 @@ from auth import views as auth_views
 from .views import home_page_view, About_view
 
 urlpatterns = [
-    path('', home_page_view),
+    path('', home_page_view,name="home"),
     path('login/', auth_views.login_view),
     path('register/', auth_views.register_view),
     path('hello-world/', home_page_view),
